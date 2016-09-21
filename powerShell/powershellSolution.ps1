0778512a09687f558fd8cf139c010a05584f13fd
iesfpiper <iesfpiper@gmail.com>
2016-09-21 01:56:25 -0400

    Add powershell solution
    Code a solution in powershell.  Definitely this was the easiest way to
    do it, took the least time, and works pretty fast.  Of course, not very
    standard.

---
+++ powerShell/powershellSolution.ps1
@@ -0,0 +1,26 @@
+﻿$webDataArray = @()
+$totalPrice = 0
+$pageNum = 1
+$webRequest = Invoke-WebRequest -uri http://shopicruit.myshopify.com/products.json?page=$pageNum
+$jsonData = $webRequest.content
+$extractedData = ConvertFrom-Json $jsonData
+while($extractedData.products.length -gt 0){
+    $webDataArray += $extractedData
+    $pageNum++
+    $webRequest = Invoke-WebRequest -uri http://shopicruit.myshopify.com/products.json?page=$pageNum
+    $jsonData = $webRequest.content
+    $extractedData = ConvertFrom-Json $jsonData
+}
+
+foreach($page in $webDataArray){
+    foreach($product in $page.products){
+        foreach($variants in $product.variants){
+            foreach($variant in $variants){
+                $variant.price
+                $totalPrice += $variant.price
+            }
+        }
+    }
+}
+
+ "Total price `$$totalPrice"
