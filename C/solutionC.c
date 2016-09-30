dc97e5d21229dd668fa79de0e9b681d8343b9801
Stephen D. Arsenault <iesfpiper@gmail.com>
2016-09-30 00:27:15 -0400

    works

--- C/solutionC.c
+++ C/solutionC.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 
 #define BUFFER_SIZE  (256 * 1024)  /* 256 KB */
+#define DEBUG 1
 
 struct write_result
 {
@@ -112,39 +113,54 @@ int main(void){
 	const char* strTitle;
 	
 	char * urlBlank = "http://shopicruit.myshopify.com/products.json?page=";
-	int page = 2;
+	int page = 1;
 	char strPage[3];
 	char url[1000];
+	double totalPrice = 0;
 	
-	sprintf(strPage, "%d", page);
-	strcpy(url, urlBlank);
-	strcat(url, strPage);
-	strcpy(url, urlBlank);
-	strcat(url, strPage);
-	
-	printf("%s", urlBlank);
-	printf("\n%s\n", url);
-	
-	text = request(url);
-	printf("\n%s\n", text);
-	root = json_loads(text, 0, &error);
 	
 	/*for reasons including how motivated I am, and perhaps it being the best choice given
 	 * the programming problem, I am chosing to grab each page and process it before grabbing
 	 * the next page
 	 */
-	products = json_object_get(root, "products");
-	product = json_array_get(products, 0);
-	prodType = json_object_get(product, "product_type");
-	strProdType = json_string_value(prodType);
-	printf("%s", strProdType);
-	variants = json_object_get(product, "variants");
-	variant = json_array_get(variants, 0);
-	price = json_object_get(variant, "price");
-	strPrice = json_string_value(price);
-	printf("\n%s\n", strPrice);
+	while(1){
+		//create URL string for current page iteration
+		sprintf(strPage, "%d", page);
+		strcpy(url, urlBlank);
+		strcat(url, strPage);
+		
+		if(DEBUG)printf("\n%s\n", url);
+		
+		//get JSON data from page
+		text = request(url);
+		//printf("\n%s\n", text);
+		
+		//parse JSON data
+		root = json_loads(text, 0, &error);
+		products = json_object_get(root, "products");
+		//check for existance of products
+		if(! (int)json_array_size(products))
+			break;
+		for(i = 0; i < (int) json_array_size(products); i++){
+			product = json_array_get(products, i);
+			prodType = json_object_get(product, "product_type");
+			strProdType = json_string_value(prodType);
+			if(DEBUG)printf("%s\n", strProdType);
+			if(!strcmp(strProdType,"Clock") || !strcmp(strProdType, "Watch")){
+				variants = json_object_get(product, "variants");
+				for(j = 0; j < (int) json_array_size(variants); j++){
+					variant = json_array_get(variants, j);
+					price = json_object_get(variant, "price");
+					strPrice = json_string_value(price);		
+					sscanf(strPrice, "%lf", &dblPrice);
+					totalPrice += dblPrice;
+					if(DEBUG)printf("price is %f", dblPrice);
+				}
+			}
+		}
+		page++;
+	}
+	printf("\nTotal Price: %lf\n", totalPrice);
 	
-	sscanf(strPrice, "%lf", &dblPrice);
-	printf("price is %f", dblPrice);
 	return 0;
 }
